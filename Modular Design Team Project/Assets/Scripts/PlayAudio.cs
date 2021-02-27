a9a54674930cff68ca6257c475238a97662937dd
SakuretsuKumare <78335074+SakuretsuKumare@users.noreply.github.com>
2021-02-26 20:00:20 -0500

    Scripts & Materials Added
    Muahahahah you have to pull this.

---
+++ Modular Design Team Project/Assets/Scripts/PlayAudio.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAudio : MonoBehaviour
+{
+    public AudioClip voiceClip;
+    private bool hasBeenPlayed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !hasBeenPlayed)
+        {
+            other.gameObject.GetComponent<AudioSource>().Stop();
+            other.gameObject.GetComponent<AudioSource>().PlayOneShot(voiceClip, 1.0f);
+            hasBeenPlayed = true;
+        }
+    }
+}
---
+++ Modular Design Team Project/Assets/Scripts/TeleportWait.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportWait : MonoBehaviour
+{
+    private GameObject player;
+    public Transform teleportTo;
+    public float waitSeconds;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Invoke("TeleportPlayer", waitSeconds);
+        }
+    }
+
+    public void TeleportPlayer()
+    {
+        player.transform.position = teleportTo.transform.position;
+        // Set's the player's transform
+        player.transform.rotation = Quaternion.identity; // or teleportTo.transform.rotation
+        // Set's the camera's transform
+        Camera.main.transform.rotation = Quaternion.identity;
+    }
+}
